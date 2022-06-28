662c5c2ebe083e0dc1ed6ccec45e0d00bd72df33
tatamibox <tat4mi@gmail.com>
2022-06-28 10:44:42 -0700

    add create and edit functionality

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Mongo connection open');
+    })
+    .catch(err => {
+        console.log("oh no, Mongo error", err)
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.listen(3000, () => {
-    console.log('Express server successfully started on port 3000.')
+    console.log('Express server successfully started on port 3000.');
 })
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 
 app.get('/', (req, res) => {
     res.render('home');
 })
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`)
+})
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
