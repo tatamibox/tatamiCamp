82d1c864dc384721534f5b063ecbc49de0fdfa7e
tatamibox <tat4mi@gmail.com>
2022-06-28 09:54:52 -0700

    set up basic express app

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.listen(3000, () => {
+    console.log('Express server successfully started on port 3000.')
+})
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
