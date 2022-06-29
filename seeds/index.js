5383820ef8d54acc5ecb28c3a1cad2dd6a893800
tatamibox <tat4mi@gmail.com>
2022-06-29 12:05:23 -0700

    error handling

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus reiciendis reprehenderit ea quo vitae nemo cum quia totam iste aperiam, autem dolores dolor accusamus facere porro minima est debitis facilis.',
+            price
         })
         await camp.save();
     }
